57d0f8f8ccd0b0e383ad37a0d699538ee7eadacc
tomh73444 <voletisaikiran94@gmail.com>
2020-10-06 09:46:26 +0530

    another commit

--- src/test/java/data/Sample.java
+++ src/test/java/data/Sample.java
@@ -3,10 +3,16 @@ package data;
 public class Sample {
 
 
+
 	public void samplemethod() {
 	}
 	
 	public void samplemethod1() {
 		System.out.println("demo");
 	}
-	}
+	
+
+public void demo() {
+	System.out.println("sample");
+}
+}
